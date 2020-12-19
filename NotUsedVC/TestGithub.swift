f6e148c03eb51a25ad414ddde2b87f822555658a
Zach <venanziz@gmail.com>
2020-12-19 09:45:15 -0500

    Trying to push changes to test file

--- NotUsedVC/TestGithub.swift
+++ NotUsedVC/TestGithub.swift
@@ -8,5 +8,12 @@
 import UIKit
 
 class TestGithub: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
     
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
