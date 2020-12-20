cd2849ff0613ec1f12d5443012ef00f709c981c6
Zach <venanziz@gmail.com>
2020-12-19 22:12:51 -0500

    Added Buttons with connections to SearchVC and added searchvc cell

---
+++ Jup/SearchCell.swift
@@ -0,0 +1,26 @@
+//
+//  lTableViewCell.swift
+//  Jup
+//
+//  Created by Zach Venanzi on 12/19/20.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    @IBOutlet weak var SCSongTitle: UILabel!
+    @IBOutlet weak var SCSongAlbumArt: UIImageView!
+    @IBOutlet weak var SCSongArtist: UILabel!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Jup/VCs/SearchVC.swift
+++ Jup/VCs/SearchVC.swift
@@ -13,11 +13,13 @@ class SearchVC: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //This makes the keyboard show up immediately after seque
         musicSearch.becomeFirstResponder()
         
     }
-        
-    
+    @IBOutlet weak var SpotifyLibraryBut: UIButton!
+    @IBOutlet weak var AppleMusicLibraryBut: UIButton!
+    @IBOutlet weak var SearhTableView: UITableView!
     @IBOutlet weak var musicSearch: UISearchBar!
        
 }
