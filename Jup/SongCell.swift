1e1ef5ccf9131bfc7583757aadb63fd08297acce
Zach <venanziz@gmail.com>
2020-12-24 14:33:50 -0500

    queue cell count label autosizing fix

--- Jup/SongCell.swift
+++ Jup/SongCell.swift
@@ -12,15 +12,16 @@ class SongCell: UITableViewCell {
     @IBOutlet var songCellLabel:UILabel!
     @IBOutlet var aristCellLabel:UILabel!
     @IBOutlet weak var songCellAlbumImage: UIImageView!
-    @IBOutlet weak var LikeButtonCountLabel: UILabel!
-    @IBOutlet weak var LikeButton: UIButton!
+    @IBOutlet weak var likeButtonCountLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
     
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        likeButtonCountLabel.adjustsFontSizeToFitWidth = true
         // Initialization code
     }
-
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
