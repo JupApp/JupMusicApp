210c486eb149c3a157684ec2628c9a12c8ec133f
Zach <venanziz@gmail.com>
2020-12-31 20:26:11 -0500

    participantSettingsVC table and cell stuff

--- Jup/VCs/ParticipantSettingsVC.swift
+++ Jup/VCs/ParticipantSettingsVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ParticipantSettingsVC: UIViewController {
+class ParticipantSettingsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var joinableQueuesTable: UITableView!
@@ -15,12 +15,28 @@ class ParticipantSettingsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let cellNib = UINib(nibName: "joinableQueueCell", bundle: nil)
+        joinableQueuesTable.register(cellNib, forCellReuseIdentifier: "joinableQueueCell")
+        joinableQueuesTable.register(joinableQueueCell.self, forCellReuseIdentifier: "joinableQueueCell")
+        
+        joinableQueuesTable.delegate = self
+        joinableQueuesTable.dataSource = self
+        joinableQueuesTable.backgroundColor = UIColor.clear
+        self.joinableQueuesTable.layer.cornerRadius = 10
+        joinableQueuesTable.separatorStyle = .none
+
+
     }
-    
- 
-
-    
-
-    
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(
+            withIdentifier: "joinableQueueCell", for: indexPath) as? joinableQueueCell
+    if cell == nil {
+        cell = joinableQueueCell(style:.default, reuseIdentifier: "joinableQueueCell")
+    }
+    return cell!
+    }
 }
+
